3c67776f1ffe70f4e6a0c275c483653e2b1add9d
mvcanalpp <volkancanalp.alkanhukuk@gmail.com>
2026-08-31 16:58:56 +0300

    feat: add kesif management PWA prototype

---
+++ kesif/sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',event=>event.waitUntil(caches.open('mvc-kesif-v1').then(cache=>cache.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request))));
