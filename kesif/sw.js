a27df48010543156ed729891f4f47df341988282
mvcanalpp <volkancanalp.alkanhukuk@gmail.com>
2026-09-01 16:36:15 +0300

    Refresh cached inspection app assets

--- kesif/sw.js
+++ kesif/sw.js
@@ -1,4 +1,4 @@
-self.addEventListener('install', event => event.waitUntil(caches.open('mvc-kesif-v2').then(cache => cache.addAll(['./','./index.html','./app.js','./manifest.webmanifest']))));
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))));
+self.addEventListener('install', event => event.waitUntil(caches.open('mvc-kesif-v3').then(cache => cache.addAll(['./','./index.html','./manifest.webmanifest']))));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== 'mvc-kesif-v3').map(key => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', event => { if (event.request.method !== 'GET') return; event.respondWith(fetch(event.request).catch(() => caches.match(event.request))); });
 self.addEventListener('notificationclick', event => { event.notification.close(); event.waitUntil(clients.matchAll({ type:'window', includeUncontrolled:true }).then((windows) => windows[0] ? windows[0].focus() : clients.openWindow('./'))); });
